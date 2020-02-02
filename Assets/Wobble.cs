8885c8509dcb21db1f24b08c8c2ec7eb28138745
Erik Nyquist <eknyquist@gmail.com>
2020-02-02 13:57:30 -0800

    Add script to make pickup boxes wobble

---
+++ Assets/Wobble.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wobble : MonoBehaviour
+{
+    float speed = 10f;
+    float amplitude = 0.1f;
+
+    float startY;
+    
+    void Start()
+    {
+        startY = gameObject.transform.position.y;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currPos = gameObject.transform.position;
+        currPos.y = startY + (Mathf.Sin(Time.time * speed) * amplitude);
+        gameObject.transform.position = currPos;
+    }
+}
